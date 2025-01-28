0899e9779f13a37fef99927291dc8f976d5d7dff
MalikTahir <7132757@gmail.com>
2025-01-29 00:01:06 +0500

    07-commitlint

--- 07-commit_lint/commitlint.config.js
+++ 07-commit_lint/commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "extends" : ["@commetlint/cli","@commitlint/config-conventional"],
+    "extends" : ["@commitlint/cli","@commitlint/config-conventional"],
     "rules": {
         "type-enum" :[
             2,
